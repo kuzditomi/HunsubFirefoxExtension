f21cc4b1109b153c6e8f418c8bdfc80103908b0b
Küzdi Tamás <kuzditomi@gmail.com>
2014-01-18 01:49:48 +0100

    fuck this shit, going greasemonkey

---
+++ src/hunsubextension.user.js
@@ -0,0 +1,186 @@
+// ==UserScript==
+// @name        d2jsp Hunsub Extension
+// @namespace   kuzditomi.hunsub.ffextension
+// @description d2jsp Hunsub Extension
+// @include     about:home
+// @include     *.d2jsp.org/*
+// @version     1
+// @grant    GM_getValue
+// @grant    GM_setValue
+// @grant    GM_addStyle
+// ==/UserScript==
+var a = document.querySelector('.tbb'),
+    userDivs,
+    blockQuotes = false;
+if(a){
+    userDivs = document.querySelectorAll('form[name=REPLIER] > dl');
+    blockUsers();
+    addButtons();
+}
+addSettings();
+
+function addButtons(){
+    for(var i =0;i < userDivs.length-1;i++){
+        var userDiv = userDivs[i];
+        var links = userDiv.querySelector('.fR.links');
+        var blockLink = document.createElement('a');
+
+        if(!links || !blockLink)
+            continue;
+
+        var nameAnchor = userDiv.querySelector('dt a');
+        var start = nameAnchor.href.indexOf('user.php?i=');
+        var userId = nameAnchor.href.substring(start+11,nameAnchor.length);
+        var userName = nameAnchor.textContent;
+        (function(userId,userName){
+            blockLink.addEventListener('click',function() {
+                blockUser(userId,userName);
+            });
+        })(userId,userName);
+
+        blockLink.textContent = "Block";
+        blockLink.style.cursor = "pointer";
+        links.appendChild(blockLink);
+    }
+}
+
+function blockUsers(){
+    console.log('starting');
+    var blockQuotes = GM_getValue('blockQuotes'),
+        blockedUsers = GM_getValue('blockedUsers') || {};
+
+    if(!blockedUsers)
+        return;
+    else
+        blockedUsers = JSON.parse(blockedUsers);
+
+    userDivs = document.querySelectorAll('form[name=REPLIER] > dl');
+    for(var i =0;i < userDivs.length-1;i++){
+        var userDiv = userDivs[i];
+        var nameAnchor = userDiv.querySelector('dt a');
+        var start = nameAnchor.href.indexOf('user.php?i=');
+        var userId = nameAnchor.href.substring(start+11,nameAnchor.length);
+        if(blockedUsers[userId]){
+            userDiv.parentNode.removeChild(userDiv);
+        }else if(blockQuotes){
+            var quoteDivs = userDiv.querySelectorAll('.quote1');
+            for(var j = 0;j<quoteDivs.length;j++){
+                var quoteDiv = quoteDivs[j];
+                var userName = quoteDiv.textContent.match(/Quote\s\(([^\s]+)/)[1];
+                for(var prop in blockedUsers) {
+                    if(blockedUsers.hasOwnProperty(prop)) {
+                        if(blockedUsers[prop] === userName) {
+                            var quote = quoteDiv.nextSibling;
+                            quoteDiv.parentNode.removeChild(quoteDiv);
+                            quote.parentNode.removeChild(quote);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function blockUser(userId, userName){
+    var blockedUsers = GM_getValue('blockedUsers');
+    if(blockedUsers)
+        blockedUsers = JSON.parse(blockedUsers);
+    else
+        blockedUsers = {};
+
+    blockedUsers[userId] = userName;
+    GM_setValue('blockedUsers',JSON.stringify(blockedUsers));
+    blockUsers();
+}
+
+function addSettings() {
+    var leftBar = document.querySelector('div.bar > ul.barL'),
+        blockedSettings;
+
+    blockedSettings = document.createElement('li');
+    blockedSettings.innerHTML = '<a style="cursor: pointer;">Blocked users</a>';
+    blockedSettings.addEventListener('click',showBlockedSettings);
+
+    leftBar.appendChild(blockedSettings);
+}
+
+
+function showBlockedSettings() {
+    var container = document.createElement('div'),
+        leftBar = document.querySelector('div.bar > ul.barL'),
+        usersdiv,
+        i;
+
+    container.innerHTML =
+        '<div class="close" id="closeButton">close[X]</div>' +
+        '<h1>D2jsp post blocker</h1>' +
+        '<div id="blockedUsers">' +
+        '</div>';
+    container.id = "settingsContainer";
+    leftBar.appendChild(container);
+
+    var blockedUsers = GM_getValue('blockedUsers');
+    if(blockedUsers)
+        blockedUsers = JSON.parse(blockedUsers);
+    else
+        blockedUsers = {};
+
+    usersdiv = document.getElementById('blockedUsers');
+    for(var i in blockedUsers){
+        var userDiv = document.createElement('div');
+        userDiv.setAttribute('class','user');
+        userDiv.setAttribute('id',blockedUsers[i]);
+
+        var userNameSpan = document.createElement('span');
+        userNameSpan.textContent = blockedUsers[i];
+
+        var enableButton = document.createElement('input');
+        enableButton.setAttribute('type','button');
+        enableButton.setAttribute('class','enable');
+        (function(i){
+            enableButton.addEventListener('click',function() {
+                var deleteDiv = document.getElementById(blockedUsers[i]);
+                delete blockedUsers[i];
+                GM_setValue('blockedUsers',JSON.stringify(blockedUsers));
+            });
+        })(i);
+        enableButton.setAttribute('value',"unblock");
+
+        userDiv.appendChild(userNameSpan);
+        userDiv.appendChild(enableButton);
+        usersdiv.appendChild(userDiv);
+    }
+
+    document.getElementById('closeButton').addEventListener('click',function(){
+        var settingsContainer = document.getElementById('settingsContainer');
+        settingsContainer.parentNode.removeChild(settingsContainer);
+    });
+
+    GM_addStyle('' +
+        '#settingsContainer {' +
+            'background: #FFFFFF;' +
+            'border: 1px solid #000000;' +
+            'height: 30em;' +
+            'position: absolute;' +
+            'width: 40ex;' +
+            'z-index: 10;' +
+            'padding: 10px;' +
+        '}' +
+        '#settingsContainer > div#blockedUsers {' +
+            'max-height: 20em;' +
+            'overflow: auto;' +
+        '}' +
+        '.user > span {' +
+            'display: inline-block;' +
+            'min-width: 25ex;' +
+        '}' +
+        '#settingsContainer > h1{' +
+            'margin-top:0;' +
+        '}' +
+        '#settingsContainer > .close {' +
+            'position: absolute;' +
+            'top:.1em;' +
+            'right:.1em;' +
+            'cursor: pointer;' +
+        '}');
+}
